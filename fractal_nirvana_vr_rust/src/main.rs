bd88cc601adf49633dfd3a89c0cea7c1245c5dc1
Steven Brodhead <keldor314@gmail.com>
2018-08-28 02:45:04 -0600

    Basic window.

--- fractal_nirvana_vr_rust/src/main.rs
+++ fractal_nirvana_vr_rust/src/main.rs
@@ -1,3 +1,21 @@
+extern crate winit;
+
 fn main() {
-    println!("Hello, world!");
+    let mut events_loop = winit::EventsLoop::new();
+    let _companion_window = winit::WindowBuilder::new()
+        .with_title("Fractal Nirvava VR")
+        .with_dimensions((1080,1200).into())
+        .with_resizable(false)
+        .build(&events_loop)
+        .unwrap();
+
+    events_loop.run_forever(|event| {
+        match event {
+            winit::Event::WindowEvent {
+                event: winit::WindowEvent::CloseRequested,
+                ..
+            } => winit::ControlFlow::Break,
+            _ => winit::ControlFlow::Continue,
+        }
+    });
 }
